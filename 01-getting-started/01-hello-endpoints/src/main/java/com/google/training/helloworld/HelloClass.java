f6a9f3e0470f3eb6368dccb2d52b2f9e3cf25c6b
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-03-11 17:31:09 +0100

    Add a two-argument constructor to our POJO HelloClass

--- 01-getting-started/01-hello-endpoints/src/main/java/com/google/training/helloworld/HelloClass.java
+++ 01-getting-started/01-hello-endpoints/src/main/java/com/google/training/helloworld/HelloClass.java
@@ -7,13 +7,33 @@ public class HelloClass {
 
     private String message = "Hello World";
 
+    /**
+     * Default constructor
+     */
     public HelloClass () {
     }
 
+    /**
+     * One-argument constructor
+     * @param name the name
+     */
     public HelloClass (String name) {
         this.message = "Hello " + name + "!";
     }
 
+    /**
+     * Two-argument constructor
+     * @param name the name
+     * @param period the period
+     */
+    public HelloClass(String name, String period) {
+        this.message = String.format("Good %s %s!", period, name);
+    }
+
+    /**
+     * Return this greeting's message
+     * @return the message
+     */
     public String getMessage() {
         return message;
     }
