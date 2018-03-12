1d50c2ca786245a9b98f804d123ed9a3f27fd0a5
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-03-12 09:45:58 +0100

    Update saveProfile method with ProfileForm capabilities

--- 01-getting-started/03-conference-central/src/main/java/com/google/devrel/training/conference/spi/ConferenceApi.java
+++ 01-getting-started/03-conference-central/src/main/java/com/google/devrel/training/conference/spi/ConferenceApi.java
@@ -14,73 +14,72 @@ import com.google.devrel.training.conference.form.ProfileForm.TeeShirtSize;
 import com.googlecode.objectify.Key;
 
 /**
- * Defines conference APIs.
+ * This class defines the conference APIs.
+ * @author Solange U. Gasengayire
  */
-@Api(name = "conference", version = "v1", scopes = { Constants.EMAIL_SCOPE }, clientIds = {
-        Constants.WEB_CLIENT_ID, Constants.API_EXPLORER_CLIENT_ID }, description = "API for the Conference Central Backend application.")
+@Api(name = "conference",
+        version = "v1",
+        scopes = {Constants.EMAIL_SCOPE},
+        clientIds = {Constants.WEB_CLIENT_ID, Constants.API_EXPLORER_CLIENT_ID},
+        description = "API for the Conference Central Backend application.")
 public class ConferenceApi {
 
-    /*
-     * Get the display name from the user's email. For example, if the email is
-     * lemoncake@example.com, then the display name becomes "lemoncake."
+    /**
+     * Get the display name from the user's email.
+     * For example, if the email is lemoncake@example.com,
+     * then the display name becomes "lemoncake."
+     * @param email the email input
+     * @return the display name
      */
     private static String extractDefaultDisplayNameFromEmail(String email) {
         return email == null ? null : email.substring(0, email.indexOf("@"));
     }
 
     /**
-     * Creates or updates a Profile object associated with the given user
-     * object.
-     *
-     * @param user
-     *            A User object injected by the cloud endpoints.
-     * @param profileForm
-     *            A ProfileForm object sent from the client form.
-     * @return Profile object just created.
-     * @throws UnauthorizedException
-     *             when the User object is null.
+     * Creates or updates a Profile object associated with the given user object.
+     * user a User object injected by the cloud endpoints.
+     * @param profileForm a ProfileForm object sent from the client form.
+     * @return the profile object just created.
+     * @throws UnauthorizedException when the User object is null.
      */
-
-    // Declare this method as a method available externally through Endpoints
     @ApiMethod(name = "saveProfile", path = "profile", httpMethod = HttpMethod.POST)
-    // The request that invokes this method should provide data that
-    // conforms to the fields defined in ProfileForm
+    public Profile saveProfile(ProfileForm profileForm) throws UnauthorizedException {
+        // The @ApiMethod annotation declares this method as a method available externally through Endpoints.
+        // The request that invokes this method should provide data that conforms to the fields defined in ProfileForm.
 
-    // TODO 1 Pass the ProfileForm parameter
-    // TODO 2 Pass the User parameter
-    public Profile saveProfile() throws UnauthorizedException {
+        // TODO 1 Pass the ProfileForm parameter
+        // TODO 2 Pass the User parameter
 
-        String userId = null;
-        String mainEmail = null;
-        String displayName = "Your name will go here";
+        // TODO 2 If the user is not logged in, throw an UnauthorizedException
+//        if (user == null) {
+//            throw new UnauthorizedException("Authorization required");
+//        }
+
         TeeShirtSize teeShirtSize = TeeShirtSize.NOT_SPECIFIED;
 
-        // TODO 2
-        // If the user is not logged in, throw an UnauthorizedException
+        // TODO 1 Set the teeShirtSize to the value sent by the ProfileForm if sent, otherwise leave it as is
+        if (profileForm.getTeeShirtSize() != null) {
+            teeShirtSize = profileForm.getTeeShirtSize();
+        }
 
-        // TODO 1
-        // Set the teeShirtSize to the value sent by the ProfileForm, if sent
-        // otherwise leave it as the default value
+        // TODO 2 Get the userId and mainEmail
+        String userId = null; //user.getUserId();
+        String mainEmail = null; //user.getEmail();
 
-        // TODO 1
-        // Set the displayName to the value sent by the ProfileForm, if sent
-        // otherwise set it to null
+        // TODO 1 Set the displayName to the value sent by the ProfileForm if sent, otherwise set it to null
+        String displayName = profileForm.getDisplayName();
 
-        // TODO 2
-        // Get the userId and mainEmail
+        // TODO 2 If the displayName is null, set it to default value based on the user's email
+//        if (displayName == null) {
+//            displayName = extractDefaultDisplayNameFromEmail(mainEmail);
+//        }
 
-        // TODO 2
-        // If the displayName is null, set it to default value based on the user's email
-        // by calling extractDefaultDisplayNameFromEmail(...)
-
-        // Create a new Profile entity from the
-        // userId, displayName, mainEmail and teeShirtSize
+        // Create a new Profile entity from the userId, displayName, mainEmail and teeShirtSize
         Profile profile = new Profile(userId, displayName, mainEmail, teeShirtSize);
 
-        // TODO 3 (In Lesson 3)
-        // Save the Profile entity in the datastore
+        // TODO 3 (In Lesson 3) Save the Profile entity in the datastore
 
-        // Return the profile
+        // Return the newly created profile
         return profile;
     }
 
@@ -100,8 +99,7 @@ public class ConferenceApi {
             throw new UnauthorizedException("Authorization required");
         }
 
-        // TODO
-        // load the Profile Entity
+        // TODO load the Profile Entity by initializing these variables
         String userId = ""; // TODO
         Key key = null; // TODO
         Profile profile = null; // TODO load the Profile entity
