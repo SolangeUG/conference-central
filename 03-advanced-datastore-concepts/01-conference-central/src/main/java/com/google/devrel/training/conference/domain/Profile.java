cccb30cbbfb6cff30cea58fddeffe94061cbc249
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-03-14 15:35:20 +0100

    Setup updated Profile class for this project

--- 03-advanced-datastore-concepts/01-conference-central/src/main/java/com/google/devrel/training/conference/domain/Profile.java
+++ 03-advanced-datastore-concepts/01-conference-central/src/main/java/com/google/devrel/training/conference/domain/Profile.java
@@ -4,24 +4,28 @@ import com.google.devrel.training.conference.form.ProfileForm.TeeShirtSize;
 import com.googlecode.objectify.annotation.Entity;
 import com.googlecode.objectify.annotation.Id;
 
-
-// TODO indicate that this class is an Entity
+/**
+ * This class represents a Profile entity.
+ * @author Solange U. Gasengayire
+ */
 @Entity
 public class Profile {
-    String displayName;
-    String mainEmail;
-    TeeShirtSize teeShirtSize;
+    // COMPLETED indicate that this class is an Entity
 
-    // TODO indicate that the userId is to be used in the Entity's key
-    @Id String userId;
-    
+    private String displayName;
+    private String mainEmail;
+    private TeeShirtSize teeShirtSize;
+
+    // COMPLETED indicate that the userId is to be used in the Entity's key
+    @Id
+    private String userId;
+
     /**
      * Public constructor for Profile.
      * @param userId The user id, obtained from the email
      * @param displayName Any string user wants us to display him/her on this system.
      * @param mainEmail User's main e-mail address.
      * @param teeShirtSize The User's tee shirt size
-     * 
      */
     public Profile (String userId, String displayName, String mainEmail, TeeShirtSize teeShirtSize) {
         this.userId = userId;
@@ -29,33 +33,43 @@ public class Profile {
         this.mainEmail = mainEmail;
         this.teeShirtSize = teeShirtSize;
     }
-    
+
+    /**
+     * Return the display name associated with this profile
+     * @return display name
+     */
     public String getDisplayName() {
         return displayName;
     }
 
+    /**
+     * Return the main email address associated with this profile
+     * @return the main email address
+     */
     public String getMainEmail() {
         return mainEmail;
     }
 
+    /**
+     * Return the tee-shirt size associated with this profile
+     * @return the shirt size
+     */
     public TeeShirtSize getTeeShirtSize() {
         return teeShirtSize;
     }
 
+    /**
+     * Return the user ID associated with this profile
+     * @return the userID
+     */
     public String getUserId() {
         return userId;
     }
 
     /**
-     * Just making the default constructor private.
-     */
-    private Profile() {}
-    
-    /**
-     * Update the Profile with the given displayName and teeShirtSize
-     *
-     * @param displayName
-     * @param teeShirtSize
+     * Update this profile's properties
+     * @param displayName the new display name value
+     * @param teeShirtSize the new tee shirt size value
      */
     public void update(String displayName, TeeShirtSize teeShirtSize) {
         if (displayName != null) {
@@ -65,5 +79,10 @@ public class Profile {
             this.teeShirtSize = teeShirtSize;
         }
     }
+
+    /**
+     * A private default constructor.
+     */
+    private Profile() {}
 
 }
