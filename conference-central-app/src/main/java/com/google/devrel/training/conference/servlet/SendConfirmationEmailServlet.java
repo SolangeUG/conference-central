3a3833affbfa85c33ebd77d34b71f55660045746
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-04-13 13:38:51 +0200

    Refactor, update and configure tasks and cron jobs using servlets

--- conference-central-app/src/main/java/com/google/devrel/training/conference/servlet/SendConfirmationEmailServlet.java
+++ conference-central-app/src/main/java/com/google/devrel/training/conference/servlet/SendConfirmationEmailServlet.java
@@ -2,11 +2,6 @@ package com.google.devrel.training.conference.servlet;
 
 import com.google.appengine.api.utils.SystemProperty;
 
-import java.io.IOException;
-import java.util.Properties;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Session;
@@ -17,9 +12,15 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
- * A servlet for sending a notification e-mail.
+ * A servlet for sending out email confirmations
+ * when a conference is created, using task queues.
+ * @author Solange U. Gasengayire
  */
 public class SendConfirmationEmailServlet extends HttpServlet {
 
@@ -27,26 +28,34 @@ public class SendConfirmationEmailServlet extends HttpServlet {
             SendConfirmationEmailServlet.class.getName());
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        String email = request.getParameter("email");
-        String conferenceInfo = request.getParameter("conferenceInfo");
-        Properties props = new Properties();
-        Session session = Session.getDefaultInstance(props, null);
-        String body = "Hi, you have created a following conference.\n" + conferenceInfo;
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        // COMPLETED: use a task to send a confirmation email when someone creates a conference
+        String email = req.getParameter("email");
+        String conferenceInfo = req.getParameter("conferenceInfo");
+
+        Properties properties = new Properties();
+        Session session = Session.getDefaultInstance(properties, null);
+        String body = "Hi, you have created the following conference. \n" + conferenceInfo;
+
         try {
-            Message message = new MimeMessage(session);
+
             InternetAddress from = new InternetAddress(
                     String.format("noreply@%s.appspotmail.com",
-                            SystemProperty.applicationId.get()), "Conference Central");
+                            SystemProperty.applicationId.get()),
+                    "Conference Central");
+            InternetAddress to = new InternetAddress(email, "");
+
+            Message message = new MimeMessage(session);
             message.setFrom(from);
-            message.addRecipient(Message.RecipientType.TO, new InternetAddress(email, ""));
+            message.addRecipient(Message.RecipientType.TO, to);
             message.setSubject("You created a new Conference!");
             message.setText(body);
             Transport.send(message);
-        } catch (MessagingException e) {
-            LOG.log(Level.WARNING, String.format("Failed to send an mail to %s", email), e);
-            throw new RuntimeException(e);
+
+        } catch (MessagingException exception) {
+            LOG.log(Level.WARNING, String.format("Failed to send an email to %s", email), exception);
+            throw new ServletException(exception);
         }
     }
 }
