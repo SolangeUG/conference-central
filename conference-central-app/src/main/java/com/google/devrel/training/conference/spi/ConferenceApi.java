fa9431003b0a3b2791856168a58987907a177810
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-04-13 14:07:29 +0200

    Refactor and update documentation for our final ConferenceApi class

--- conference-central-app/src/main/java/com/google/devrel/training/conference/spi/ConferenceApi.java
+++ conference-central-app/src/main/java/com/google/devrel/training/conference/spi/ConferenceApi.java
@@ -25,6 +25,7 @@ import com.google.devrel.training.conference.form.ConferenceForm;
 import com.google.devrel.training.conference.form.ConferenceQueryForm;
 import com.google.devrel.training.conference.form.ProfileForm;
 import com.google.devrel.training.conference.form.ProfileForm.TeeShirtSize;
+import com.google.devrel.training.conference.service.OfyService;
 import com.googlecode.objectify.Key;
 import com.googlecode.objectify.Objectify;
 import com.googlecode.objectify.Work;
@@ -37,27 +38,37 @@ import java.util.logging.Logger;
 import javax.inject.Named;
 
 /**
- * Defines conference APIs.
+ * This class defines the first steps of the conference app APIs.
+ * @author Solange U. Gasengayire
  */
-
-@Api(
-        name = "conference",
+@Api(name = "conference",
         version = "v1",
-        scopes = { Constants.EMAIL_SCOPE },
-        clientIds = { Constants.WEB_CLIENT_ID, Constants.ANDROID_CLIENT_ID,
-                Constants.API_EXPLORER_CLIENT_ID},
+        scopes = {Constants.EMAIL_SCOPE},
+        clientIds = {
+                Constants.WEB_CLIENT_ID,
+                Constants.API_EXPLORER_CLIENT_ID,
+                Constants.ANDROID_CLIENT_ID},
         audiences = {Constants.ANDROID_AUDIENCE},
-        description = "Conference Central API for creating and querying conferences," +
-                " and for creating and getting user Profiles"
-)
+        description = "API for the Conference Central Backend application.")
 public class ConferenceApi {
 
     private static final Logger LOG = Logger.getLogger(ConferenceApi.class.getName());
 
+    /**
+     * Get the display name from the user's email.
+     * For example, if the email is lemoncake@example.com, then the display name becomes "lemoncake."
+     * @param email the email input
+     * @return the display name
+     */
     private static String extractDefaultDisplayNameFromEmail(String email) {
         return email == null ? null : email.substring(0, email.indexOf("@"));
     }
 
+    /**
+     * Gets the Profile entity for the current user or creates it if it doesn't exist
+     * @param user the current user
+     * @return user's Profile
+     */
     private static Profile getProfileFromUser(User user, String userId) {
         // First fetch it from the datastore.
         Profile profile = ofy().load().key(
@@ -72,28 +83,33 @@ public class ConferenceApi {
     }
 
     /**
-     * This is an ugly workaround for null userId for Android clients.
-     *
-     * @param user A User object injected by the cloud endpoints.
-     * @return the App Engine userId for the user.
+     * Ugly workaround for null or empty userId from Android clients.
+     * @param user a User object injected by the cloud endpoints from Android clients
+     * @return an App Engine userId for the user
      */
     private static String getUserId(User user) {
         String userId = user.getUserId();
         if (userId == null) {
             LOG.info("userId is null, so trying to obtain it from the datastore.");
+
             AppEngineUser appEngineUser = new AppEngineUser(user);
             ofy().save().entity(appEngineUser).now();
+
             // Begin new session for not using session cache.
             Objectify objectify = ofy().factory().begin();
             AppEngineUser savedUser = objectify.load().key(appEngineUser.getKey()).now();
+
             userId = savedUser.getUser().getUserId();
+
             LOG.info("Obtained the userId: " + userId);
         }
         return userId;
     }
 
     /**
-     * Just a wrapper for Boolean.
+     * Utility class for wrapping boolean values.
+     * This is because endpoint functions must return objects,
+     * they can't return simple data type objects such as String, Long or Boolean.
      */
     public static class WrappedBoolean {
 
@@ -166,11 +182,10 @@ public class ConferenceApi {
     }
 
     /**
-     * Returns a Profile object associated with the given user object. The cloud endpoints system
-     * automatically inject the User object.
-     *
-     * @param user A User object injected by the cloud endpoints.
-     * @return Profile object.
+     * Return a Profile object associated with the given user object.
+     * The cloud endpoints system automatically inject the User object.
+     * @param user a User object injected by the cloud endpoints.
+     * @return the Profile object associated with the user
      * @throws UnauthorizedException when the User object is null.
      */
     @ApiMethod(name = "getProfile", path = "profile", httpMethod = HttpMethod.GET)
@@ -182,11 +197,10 @@ public class ConferenceApi {
     }
 
     /**
-     * Creates or updates a Profile object associated with the given user object.
-     *
-     * @param user A User object injected by the cloud endpoints.
-     * @param profileForm A ProfileForm object sent from the client form.
-     * @return Profile object just created.
+     * Create or update a Profile object associated with the given user object.
+     * @param user a User object injected by the cloud endpoints.
+     * @param profileForm a ProfileForm object sent from the client form.
+     * @return the profile object just created.
      * @throws UnauthorizedException when the User object is null.
      */
     @ApiMethod(name = "saveProfile", path = "profile", httpMethod = HttpMethod.POST)
@@ -231,10 +245,11 @@ public class ConferenceApi {
         }
         // Allocate Id first, in order to make the transaction idempotent.
         Key<Profile> profileKey = Key.create(Profile.class, getUserId(user));
-        final Key<Conference> conferenceKey = factory().allocateId(profileKey, Conference.class);
+        final Key<Conference> conferenceKey = OfyService.ofy().factory().allocateId(profileKey, Conference.class);
         final long conferenceId = conferenceKey.getId();
         final Queue queue = QueueFactory.getDefaultQueue();
         final String userId = getUserId(user);
+
         // Start a transaction.
         Conference conference = ofy().transact(new Work<Conference>() {
             @Override
@@ -244,6 +259,8 @@ public class ConferenceApi {
                 Conference conference = new Conference(conferenceId, userId, conferenceForm);
                 // Save Conference and Profile.
                 ofy().save().entities(conference, profile).now();
+
+                // Add "send confirmation email" task
                 queue.add(ofy().getTransaction(),
                         TaskOptions.Builder.withUrl("/tasks/send_confirmation_email")
                         .param("email", profile.getMainEmail())
@@ -255,8 +272,7 @@ public class ConferenceApi {
     }
 
     /**
-     * Updates the existing Conference with the given conferenceId.
-     *
+     * Update the existing Conference with the given conferenceId.
      * @param user A user who invokes this method, null when the user is not signed in.
      * @param conferenceForm A ConferenceForm object representing user's inputs.
      * @param websafeConferenceKey The String representation of the Conference key.
@@ -265,11 +281,7 @@ public class ConferenceApi {
      * @throws NotFoundException when there is no Conference with the given conferenceId.
      * @throws ForbiddenException when the user is not the owner of the Conference.
      */
-    @ApiMethod(
-            name = "updateConference",
-            path = "conference/{websafeConferenceKey}",
-            httpMethod = HttpMethod.PUT
-    )
+    @ApiMethod(name = "updateConference",path = "conference/{websafeConferenceKey}", httpMethod = HttpMethod.PUT)
     public Conference updateConference(final User user, final ConferenceForm conferenceForm,
                                        @Named("websafeConferenceKey")
                                        final String websafeConferenceKey)
@@ -280,6 +292,7 @@ public class ConferenceApi {
         }
         final String userId = getUserId(user);
         // Update the conference with the conferenceForm sent from the client.
+
         // Need a transaction because we need to safely preserve the number of allocated seats.
         TxResult<Conference> result = ofy().transact(new Work<TxResult<Conference>>() {
             @Override
@@ -308,34 +321,32 @@ public class ConferenceApi {
         return result.getResult();
     }
 
-    @ApiMethod(
-            name = "getAnnouncement",
-            path = "announcement",
-            httpMethod = HttpMethod.GET
-    )
+    /**
+     * A method to retrieve the latest announcement out of memcache.
+     * @return an announcement if available
+     */
+    @ApiMethod(name = "getAnnouncement", path = "announcement", httpMethod = HttpMethod.GET)
     public Announcement getAnnouncement() {
         MemcacheService memcacheService = MemcacheServiceFactory.getMemcacheService();
         Object message = memcacheService.get(Constants.MEMCACHE_ANNOUNCEMENTS_KEY);
+
+        // an announcement has been found
         if (message != null) {
             return new Announcement(message.toString());
         }
+
+        // there's no announcement
         return null;
     }
 
     /**
-     * Returns a Conference object with the given conferenceId.
-     *
+     * Returns a Conference object with the given conference key.
      * @param websafeConferenceKey The String representation of the Conference Key.
      * @return a Conference object with the given conferenceId.
      * @throws NotFoundException when there is no Conference with the given conferenceId.
      */
-    @ApiMethod(
-            name = "getConference",
-            path = "conference/{websafeConferenceKey}",
-            httpMethod = HttpMethod.GET
-    )
-    public Conference getConference(
-            @Named("websafeConferenceKey") final String websafeConferenceKey)
+    @ApiMethod(name = "getConference", path = "conference/{websafeConferenceKey}", httpMethod = HttpMethod.GET)
+    public Conference getConference(@Named("websafeConferenceKey") final String websafeConferenceKey)
             throws NotFoundException {
         Key<Conference> conferenceKey = Key.create(websafeConferenceKey);
         Conference conference = ofy().load().key(conferenceKey).now();
@@ -347,16 +358,11 @@ public class ConferenceApi {
 
     /**
      * Returns a collection of Conference Object that the user is going to attend.
-     *
      * @param user An user who invokes this method, null when the user is not signed in.
      * @return a Collection of Conferences that the user is going to attend.
      * @throws UnauthorizedException when the User object is null.
      */
-    @ApiMethod(
-            name = "getConferencesToAttend",
-            path = "getConferencesToAttend",
-            httpMethod = HttpMethod.GET
-    )
+    @ApiMethod(name = "getConferencesToAttend", path = "getConferencesToAttend", httpMethod = HttpMethod.GET)
     public Collection<Conference> getConferencesToAttend(final User user)
             throws UnauthorizedException, NotFoundException {
         // If not signed in, throw a 401 error.
@@ -377,19 +383,28 @@ public class ConferenceApi {
 
     /**
      * Queries against the datastore with the given filters and returns the result.
+     * Normally, this kind of method is supposed to get invoked by a GET HTTP method,
+     * but we do it with POST, in order to receive a conferenceQueryForm object via the POST method
      *
-     * Normally this kind of method is supposed to get invoked by a GET HTTP method,
-     * but we do it with POST, in order to receive conferenceQueryForm Object via the POST body.
-     *
-     * @param conferenceQueryForm A form object representing the query.
-     * @return A List of Conferences that match the query.
+     * @param conferenceQueryForm the actual query form, where users specify the query criteria
+     * @return a list of conferences that match the query.
      */
-    @ApiMethod(
-            name = "queryConferences",
-            path = "queryConferences",
-            httpMethod = HttpMethod.POST
-    )
+    @ApiMethod(name = "queryConferences", path = "queryConferences", httpMethod = HttpMethod.POST)
     public List<Conference> queryConferences(ConferenceQueryForm conferenceQueryForm) {
+        // find all entities of type/kind Conference
+        // Query<Conference> query = ofy().load().type(Conference.class).order("name");
+
+        // return all entities matching the user criteria in the query form
+        // Query<Conference> query = queryForm.getQuery();
+
+        /*
+         * When the Web UI displays conferences, it shows the conference organizer's display name.
+         * However, for each conference, the organizer's display name is calculated on the fly when needed,
+         * in case the organizer changes their display name.
+         * So to decrease the hits to the datastore, we can preload all the Profile entities for the users
+         * who have organized conferences.
+         */
+
         Iterable<Conference> conferenceIterable = conferenceQueryForm.getQuery();
         List<Conference> result = new ArrayList<>(0);
         List<Key<Profile>> organizersKeyList = new ArrayList<>(0);
@@ -397,24 +412,20 @@ public class ConferenceApi {
             organizersKeyList.add(Key.create(Profile.class, conference.getOrganizerUserId()));
             result.add(conference);
         }
+
         // To avoid separate datastore gets for each Conference, pre-fetch the Profiles.
         ofy().load().keys(organizersKeyList);
         return result;
     }
 
     /**
-     * Returns a list of Conferences that the user created.
-     * In order to receive the websafeConferenceKey via the JSON params, uses a POST method.
+     * Ancestor query: restrict results to conferences that descend from the parent entity.
+     * We're using a POST method here again, in order to receive a conferenceQueryForm object via the POST method
      *
-     * @param user An user who invokes this method, null when the user is not signed in.
-     * @return a list of Conferences that the user created.
-     * @throws UnauthorizedException when the user is not signed in.
+     * @param user the ancestor entity - the user who created the conferences
+     * @return a list of conferences created by the logged in user
      */
-    @ApiMethod(
-            name = "getConferencesCreated",
-            path = "getConferencesCreated",
-            httpMethod = HttpMethod.POST
-    )
+    @ApiMethod(name = "getConferencesCreated", path = "getConferencesCreated", httpMethod = HttpMethod.POST)
     public List<Conference> getConferencesCreated(final User user) throws UnauthorizedException {
         // If not signed in, throw a 401 error.
         if (user == null) {
@@ -427,13 +438,14 @@ public class ConferenceApi {
     }
 
     /**
-     * Registers to the specified Conference.
-     *
-     * @param user An user who invokes this method, null when the user is not signed in.
-     * @param websafeConferenceKey The String representation of the Conference Key.
-     * @return Boolean true when success, otherwise false
-     * @throws UnauthorizedException when the user is not signed in.
-     * @throws NotFoundException when there is no Conference with the given conferenceId.
+     * A method to allow users to register for conferences
+     * @param user the user registering for the conference
+     * @param websafeConferenceKey the conference key to register for
+     * @return a value/flag that indicates whether the operation was successful or not
+     * @throws UnauthorizedException in case the user is unidentified
+     * @throws NotFoundException in case the specified conference key is not found
+     * @throws ForbiddenException in case of any other unexpected error
+     * @throws ConflictException in case there are no seats available for the specified conference
      */
     @ApiMethod(
             name = "registerForConference",
@@ -441,14 +453,34 @@ public class ConferenceApi {
             httpMethod = HttpMethod.POST
     )
     public WrappedBoolean registerForConference(final User user,
-                                         @Named("websafeConferenceKey")
-                                         final String websafeConferenceKey)
-        throws UnauthorizedException, NotFoundException, ForbiddenException, ConflictException {
+                                                @Named("websafeConferenceKey") final String websafeConferenceKey)
+            throws UnauthorizedException, NotFoundException, ForbiddenException, ConflictException {
+
         // If not signed in, throw a 401 error.
         if (user == null) {
             throw new UnauthorizedException("Authorization required");
         }
+
+        // Get the userId
         final String userId = getUserId(user);
+
+        /*
+         * Start a transaction with Objectify
+         * Google App Engine transactions use snapshot isolation and optimistic concurrency.
+         * If multiple transactions update an entity, the first transaction to commit succeeds, the others fail.
+         *
+         * Differences between Objectify and native Datastore Transactions:
+         *   In the native Datastore API, any changes to an entity inside a transaction are not reflected to any
+         *   queries or gets made later in the transaction.
+         *
+         *   However, queries and gets inside transactions in Objectify will reflect any changes to entities made
+         *   previously in the transaction (be aware though that those changes will be lost if the transaction does not
+         *   commit successfully).
+         *
+         * Best practice:
+         *   limit the use of transactions as much as possible to entity updates rather than for queries and gets.
+         */
+
         TxResult<Boolean> result = ofy().transact(new Work<TxResult<Boolean>>() {
             @Override
             public TxResult<Boolean> run() {
@@ -479,10 +511,9 @@ public class ConferenceApi {
 
     /**
      * Unregister from the specified Conference.
-     *
      * @param user An user who invokes this method, null when the user is not signed in.
-     * @param websafeConferenceKey The String representation of the Conference Key to unregister
-     *                             from.
+     * @param websafeConferenceKey The String representation of the Conference Key
+     * to unregister from.
      * @return Boolean true when success, otherwise false.
      * @throws UnauthorizedException when the user is not signed in.
      * @throws NotFoundException when there is no Conference with the given conferenceId.
